4edc25d2ac76bbd40336d25c4d45bb7bc4615d3f
milleraustin139 <65372653+milleraustin139@users.noreply.github.com>
2025-06-21 16:32:36 -0500

    Set up front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Wordle';
+  lastKey: string = '';
+
+  readonly rows = 6;
+  readonly cols = 5;
+
+  grid: any[] = [];
+
+  Math = Math;
+
+  ngOnInit() {
+    this.grid = Array.from({ length: this.rows * this.cols });
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (/^[a-zA-Z]$/.test(event.key)) {
+      this.lastKey = event.key;
+      console.log('Key pressed:', event.key);
+    }
+  }
 }
